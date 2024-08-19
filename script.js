fd9c2d2fbb9d102b08d74a4cd105ddf8088f4e0a
ilovemec <boothev81@craighead.school.nz>
2024-08-20 10:24:52 +1200

    Random number
    I added in the random number generator and made it output the number to see if it worked.
    Got rid of the while since it didn't work.

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ function guessNumber() {
   let playersName = prompt("What is your name?")
   alert("Hi " + playersName + " Im thinking of a number between 1 and 20, inclusive")
 
-  playersGuess = ""
+  let randomNumber = Math.floor(Math.random() * (20 - 0) + 0)
+  alert(+randomNumber)
 
-  while (playersGuess == "") {
-    let playersGuess = prompt("What's your guess?")
-  }
+  let playersGuess = prompt("What's your guess?")
+
   if (playersGuess == randomNumber) {
     alert("Correct!!!")
   } else {
     alert("Wrong")
-    playersGuess = ""
   }
-  let randomNumber = Math.floor(Math.random() * (20 - 0) + 0)
 }
