75b16d579096ca45b10c45c3944a75e087bb7183
ilovemec <boothev81@craighead.school.nz>
2024-09-02 07:42:00 +1200

    Added
    Added things to make my code work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function guessNumber() {
   alert("Hello, let's play guess the number")
   let playersName = prompt("What is your name?")
-  alert("Hi " + playersName + " Im thinking of a number between 1 and 20, inclusive")
+  alert("Hi " + playersName + " im thinking of a number between 1 and 20, inclusive")
 
   let randomNumber = Math.floor(Math.random() * (20 - 0) + 0)
   alert(randomNumber)
@@ -13,7 +13,6 @@ function guessNumber() {
   }
   if (playersGuess == randomNumber) {
     alert("Correct!!!")
-    //   } else if (playersGuess != randomNumber) {
   } else {
     alert("Wrong")
   }
